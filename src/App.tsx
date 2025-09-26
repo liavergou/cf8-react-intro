4202f5b18ab4b4fa07bb8074c2961db005087a18
liavergou <liaverg@gmail.com>
2025-09-26 18:41:51 +0300

    feat: add arrow functional component with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 
 //Ονοματίζουμε με το όνομα του component και γράφουμε ολη τη λογική της Javascript. MΜπαινει ολη η λογική του routing
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
 
@@ -11,9 +12,13 @@ function App() {
   return (
     <>
         {/*<ViteIntro />  //Καλώ το component*/}
-        <ClassComponent />
-        <FunctionalComponent/>
-        <ArrowFunctionalComponent/>
+        {/*<ClassComponent />*/}
+        {/*<FunctionalComponent/>*/}
+        {/*<ArrowFunctionalComponent/>*/}
+        <ArrowFunctionalComponentWithProps title = "Is an Arrow Functional Component With Props"/>
+        <ArrowFunctionalComponentWithProps title = "2nd Title"/>
+
+
 
     </>
   )
