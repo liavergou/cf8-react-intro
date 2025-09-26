217306c50c33e1ead231f709799b0c4a0aee8cbb
liavergou <liaverg@gmail.com>
2025-09-26 08:54:27 +0300

    feat: move vite landing into a new component

---
+++ src/components/ViteIntro/ViteIntro.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from '../../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './ViteIntro.css';
+
+//ΜΠΑΙΝΕΙ ΤΟ ΟΝΟΜΑ ΤΟΥ COMPONENT και γράφουμε ολη τη λογική της Javascript
+function ViteIntro() {
+    const [count, setCount] = useState(0)
+
+
+    //στο return μπαίνει η html
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
+//--------ΜΠΑΙΝΕΙ ΤΟ ΟΝΟΜΑ ΤΟΥ COMPONENT
+export default ViteIntro
