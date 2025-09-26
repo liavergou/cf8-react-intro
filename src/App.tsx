e19098680b59ed92728024ab79dd569602df8e13
liavergou <liaverg@gmail.com>
2025-09-26 09:06:43 +0300

    fix: new installation

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 //Ονοματίζουμε με το όνομα του component και γράφουμε ολη τη λογική της Javascript. MΜπαινει ολη η λογική του routing
 function App() {
 
-
-
     //στο return μπαίνει η html
   return (
     <>
